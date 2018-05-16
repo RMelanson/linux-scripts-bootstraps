721eb7ed0ab1ae9f6a112fb960fc809990d2410b
Robin Melanson <remcogold@gmail.com>
2018-05-16 00:41:57 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+#------------------- SET THE MASTER ENVIRONMENT --------------------
+. ./env/setEnv.sh
+#------------------------ INSTALL DEV TOOLS ------------------------
+./devToolsBootstrap.sh
