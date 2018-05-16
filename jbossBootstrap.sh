5392878b1c43536aa70571b9d2d7efa0c98d2c94
Robin Melanson <remcogold@gmail.com>
2018-05-16 00:27:44 -0400

    Update jbossBootstrap.sh

--- jbossBootstrap.sh
+++ jbossBootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-currDir=$PWD
+jbossBootCurrDir=$PWD
 jbossDir=/tmp/scripts/apps/JBOSS
 git clone https://github.com/RMelanson/linux-scripts-apps-jboss.git $jbossDir
 cd $jbossDir
-./setup.sh
-cd $currDir
+. ./setup.sh
+cd $jbossBootCurrDir
