06314ee92e425fbc7d459c72f614ceeda9235923
Robin Melanson <remcogold@gmail.com>
2018-05-16 01:04:25 -0400

    Update webBootstrap.sh

--- webBootstrap.sh
+++ webBootstrap.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-currDir=$PWD
+webCurrDir=$PWD
 devToolsDir=/tmp/scripts/apps/WEB
 git clone https://github.com/RMelanson/linux-scripts-apps-web.git $devToolsDir
 cd $devToolsDir
-chmod 744 setup.sh
-./setup.sh
-cd $currDir
+. ./setup.sh
+cd $webCurrDir
