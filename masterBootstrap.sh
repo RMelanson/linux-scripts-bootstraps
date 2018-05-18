a3995230ed500040c2ed745581603b4f9bfdf197
Robin Melanson <remcogold@gmail.com>
2018-05-18 10:39:32 -0400

    Update masterBootstrap.sh

--- masterBootstrap.sh
+++ masterBootstrap.sh
@@ -6,8 +6,19 @@ yum update -y
 
 #INSTALL GIT
 yum install git -y
-#INSTALL GITHUB ACCESS KEYS
-gitHubDir=/tmp/scripts/utils/gitHub
+
+#INSTALL GITHUB CONFIGURATION
+echo host github.com                 >> ~/.ssh/config
+echo  HostName github.com            >> ~/.ssh/config
+echo "StrictHostKeyChecking no"      >> ~/.ssh/config
+echo  IdentityFile ~/.ssh/gitHub.key >> ~/.ssh/config
+echo  User git                       >> ~/.ssh/config
+
+ssh-keygen -t rsa -N "" -f ~/.ssh/gitHub.key
+echo "<Copy the following ssh public key to github settings for ssh keys>"
+cat ~/.ssh/gitHub.key.pub
+
+gitHubDir=/tmp/scripts/utils/gitHubssh-keygen -t rsa -N "" -f ~/.ssh/gitHub.key
 git clone git@github.com:RMelanson/linux-scripts-utils-gitHub-sshKeys.git $gitHubDir
 cd $gitHubDir
 . ./setup.sh
