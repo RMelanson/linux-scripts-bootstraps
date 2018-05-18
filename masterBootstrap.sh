d042e20ad4e551aab3b4f1d7f98b358e8cca9df3
Robin Melanson <remcogold@gmail.com>
2018-05-18 01:28:06 -0400

    Update masterBootstrap.sh

--- masterBootstrap.sh
+++ masterBootstrap.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 masterCurrDir=$(PWD)
 
-#------------------- INITIAL BASIC TOOLS INSTALL --------------------
+#INITIAL BASIC TOOLS INSTALL
 yum update -y
 
-#------------------- INSTALL GITHUB --------------------
+#INSTALL GIT
 yum install git -y
+#INSTALL GITHUB ACCESS KEYS
+gitHubDir=/tmp/scripts/utils/gitHub
+git clone git@github.com:RMelanson/linux-scripts-utils-gitHub-sshKeys.git $gitHubDir
+cd $gitHubDir
+. ./setup.sh
 
 #------------------- INSTALL MASTER BOOTSTRAP APPLICATIONS --------------------
 bootStrapsDir=/var/scripts/bootstraps
