9aac26480cefc71c3a2d7e6121fb6abba14b5fa9
Robin Melanson <remcogold@gmail.com>
2018-05-19 13:26:58 -0400

    Create wpBootstrap.sh

---
+++ wpBootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+wpCurrDir=$PWD
+
+#Set Cloning Properties
+pkg=devTools
+gitRepo="linux-scripts-apps-wp.git"
+installDir="/tmp/scripts/apps/WP"
+if [ "$1" = "ssh" ]; then
+   clone="git clone git@github.com:RMelanson/"
+else
+   clone="git clone https://github.com/RMelanson/"
+fi
+
+# Clone $pkg
+echo Executing $clone$gitRepo $installDir
+$clone$gitRepo $installDir
+
+# Setup $pkg
+cd $installDir
+. ./setup.sh
+
+cd $wpCurrDir
