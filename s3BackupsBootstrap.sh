674240b4365486d4e95343d7f49f633f3d7001c3
Robin Melanson <remcogold@gmail.com>
2018-05-19 13:12:07 -0400

    Create s3BackupsBootstrap.sh

---
+++ s3BackupsBootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+s3BackupCurrDir=$PWD
+
+#Set Cloning Properties
+pkg=devTools
+gitRepo="linux-scripts-bootstraps.git"
+installDir="/tmp/scripts/utils/s3Backups"
+if [ "$1" = "ssh" ]; then
+   clone="git clone git@github.com:RMelanson/"
+else
+   clone="git clone https://github.com/RMelanson/"
+fi
+
+# Clone $pkg
+echo Executing $clone$gitRepo $installDir
+$clone$gitRepo $installDir
+
+# Setup $pkg
+cd $installDir
+. ./setup.sh
+
+cd $s3BackupCurrDir
