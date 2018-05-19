3af4798df4f456f3f9c2161679f55aaa0b64620d
Robin Melanson <remcogold@gmail.com>
2018-05-19 12:49:07 -0400

    Create swApiRestBootStrap.sh

---
+++ swApiRestBootStrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+swApiRestDir=$PWD
+
+#Set Cloning Properties
+pkg=Web
+gitRepo="linux-scripts-apps-web.git"
+installDir="/tmp/scripts/apps/WEB"
+if [ "$1" = "ssh" ]; then
+   clone="git clone git@github.com:RMelanson/"
+else
+   clone="git clone https://github.com/RMelanson/"
+fi
+
+# Clone $pkg
+echo Executing $clone$gitRepo $installDir
+$clone$gitRepo $installDir
+
+# Setup $pkg
+cd $installDir
+. ./setup.sh
+
+cd $swApiRestDir
