86faa03f715a7faebe84c60195c10f7d15259738
Robin Melanson <remcogold@gmail.com>
2018-05-25 22:15:06 -0400

    Update wpBootstrap.sh

--- wpBootstrap.sh
+++ wpBootstrap.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+wpCurrDir=$PWD
+
 # Ensure script is running under root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root or under sudo"
   exit -1
 fi
 
-wpCurrDir=$PWD
+if [ -z "$1" ]; then
+   branch=master
+else
+   branch=$1
+fi
 
 #Set Cloning Properties
 pkg=devTools
