1b3632966e9968808c7da63311265e1b12425f60
Robin Melanson <remcogold@gmail.com>
2018-05-25 22:52:23 -0400

    Update mySqlBootstrap.sh

--- mySqlBootstrap.sh
+++ mySqlBootstrap.sh
@@ -18,9 +18,9 @@ pkg=mysql
 gitRepo="linux-scripts-apps-db-mysql.git"
 installDir="/tmp/scripts/db/MYSQL"
 if [ -f ~/.ssh/gitHub.key ]; then
-   clone="git clone git@github.com:RMelanson/"
+   clone="git clone -b $branch git@github.com:RMelanson/"
 else
-   clone="git clone https://github.com/RMelanson/"
+   clone="git clone -b $branch https://github.com/RMelanson/"
 fi
 
 # Clone $pkg
