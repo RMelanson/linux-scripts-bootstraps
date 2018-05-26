1a436348107a80d994067f34f4d8cbe495a65cc2
Robin Melanson <remcogold@gmail.com>
2018-05-25 22:14:02 -0400

    Update webBootstrap.sh

--- webBootstrap.sh
+++ webBootstrap.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+webCurrDir=$PWD
+
 # Ensure script is running under root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root or under sudo"
   exit -1
 fi
 
-webCurrDir=$PWD
+if [ -z "$1" ]; then
+   branch=master
+else
+   branch=$1
+fi
 
 #Set Cloning Properties
 pkg=Web
