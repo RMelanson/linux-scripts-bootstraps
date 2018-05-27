a94e89f8d9a1c108de940de383b45c26febde29e
root <root@ip-172-31-13-254.ca-central-1.compute.internal>
2018-05-27 15:40:33 +0000

    WIP

--- dev/devToolsBootstrap.sh
+++ dev/devToolsBootstrap.sh
@@ -12,11 +12,13 @@ else
 fi
 
 devToolsCurrDir=$PWD
+baseDir=/tmp/scripts
+subDir=dev
+pkg=devTools
+installDir="$baseDir/$subDir/$pkg"
+gitRepo="linux-scripts-devtools.git"
 
 #Set Cloning Properties
-pkg=devTools
-gitRepo="linux-scripts-devtools.git"
-installDir="/tmp/scripts/dev/tools"
 if [ -f ~/.ssh/gitHub.key ]; then
    clone="git clone -b $branch git@github.com:RMelanson/"
 else
