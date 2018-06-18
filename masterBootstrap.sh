c86a56a369d7b9100957d9254a33705e6a72370c
Robin Melanson <remcogold@gmail.com>
2018-06-17 23:50:55 -0400

    Update masterBootstrap.sh

--- masterBootstrap.sh
+++ masterBootstrap.sh
@@ -22,9 +22,9 @@ gitRepo="linux-scripts-bootstraps.git"
 
 installDir="/var/scripts/bootstraps"
 if [ -f ~/.ssh/gitHub.key ]; then
-   clone="git clone -b $branch git@github.com:RMelanson/"
+   clone="git clone git@github.com:RMelanson/"
 else
-   clone="git clone -b $branch https://github.com/RMelanson/"
+   clone="git clone https://github.com/RMelanson/"
 fi
 
 # Clone $pkg
