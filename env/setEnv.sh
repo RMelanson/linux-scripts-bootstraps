55f6b0c0d9f70325f7ea11185726a6e70d263a9a
Robin Melanson <robin.e.melanson@gmail.com>
2019-07-31 00:31:12 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -12,3 +12,12 @@ installDir="$parentDir/$pkg"
 
 export BASE_INSTALL=/tmp/scripts
 export SW_HOME=/home/SW_HOME
+
+pkgOwner=ec2-user
+
+echo Setting External Args
+echo These Arguments Overwrite Default Argument Settings
+for arg in "$@"; do
+  echo setArgs EXECUTING: export $arg
+  export $arg
+done
